d83764ca652ca9125760aa55ba39853bf3dedec9
hortobagyibalazs <hortobagyibala@gmail.com>
2022-02-23 15:29:32 +0100

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Revolution
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
