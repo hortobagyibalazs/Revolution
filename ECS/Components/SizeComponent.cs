11231e5c81b5de077af369adeab5b5d60389c15f
hortobagyibalazs <hortobagyibala@gmail.com>
2022-03-17 17:36:15 +0100

    Optimized renderer by a hundred fold

--- ECS/Components/SizeComponent.cs
+++ ECS/Components/SizeComponent.cs
@@ -1,8 +1,45 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
 namespace Revolution.ECS.Components
 {
-    public class SizeComponent : Component
+    public class SizeComponent : Component, INotifyPropertyChanged
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        private int _width;
+        private int _height;
+        
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
