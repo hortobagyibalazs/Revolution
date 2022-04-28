b395ebd89458d31b7d248f3712951542142d5e10
hortobagyibalazs <hortobagyibala@gmail.com>
2022-04-28 13:16:29 +0200

    Refined villager building tooltips

--- HUD/Entities/VillagerHud.cs
+++ HUD/Entities/VillagerHud.cs
@@ -13,6 +13,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Revolution.HUD.Entities
 {
@@ -40,18 +41,38 @@ namespace Revolution.HUD.Entities
 
         private FrameworkElement GetTooltipForBuilding(string name, int gold, int wood)
         {
-            var _buildingName = new Label();
-            _buildingName.Foreground = Brushes.White;
-            _buildingName.Content = name;
-            var _tooltipView = new Grid();
-            _tooltipView.Margin = new Thickness(16);
-            _tooltipView.Children.Add(_buildingName);
-            return _tooltipView;
+            var buildingNameLabel = new Label();
+            buildingNameLabel.Foreground = Brushes.White;
+            buildingNameLabel.Content = name;
+
+            var goldIcon = new Image() { Source = new BitmapImage(new Uri(@"\Assets\Images\spr_gold_coin_gui.png", UriKind.Relative)), Width=20, Height=20};
+            var woodIcon = new Image() { Source = new BitmapImage(new Uri(@"\Assets\Images\spr_tree_gui.png", UriKind.Relative)), Width=20, Height=20};
+
+            var goldLabel = new Label() { Content = gold, Foreground = Brushes.White };
+            var woodLabel = new Label() { Content = wood, Foreground = Brushes.White };
+
+            var resourceListView = new WrapPanel();
+            resourceListView.Children.Add(woodIcon);
+            resourceListView.Children.Add(woodLabel);
+            resourceListView.Children.Add(goldIcon);
+            resourceListView.Children.Add(goldLabel);
+
+            var contentHolderView = new DockPanel();
+            contentHolderView.Margin = new Thickness(16);
+            contentHolderView.Children.Add(buildingNameLabel);
+            contentHolderView.Children.Add(resourceListView);
+            DockPanel.SetDock(buildingNameLabel, Dock.Top);
+            DockPanel.SetDock(resourceListView, Dock.Bottom);
+
+            return contentHolderView;
         }
 
         private FrameworkElement CreateBuyHouseButton()
         {
-            var tooltip = GetTooltipForBuilding("House", 0, 0);
+            int wood = 0;
+            int gold = 0;
+            GetPriceForBuilding<House>(ref wood, ref gold);
+            var tooltip = GetTooltipForBuilding("House", gold, wood);
 
             var button = new HudMiniActionButton(
                     new Uri(@"\Assets\Images\spr_farm_button.png", UriKind.Relative),
@@ -65,6 +86,7 @@ namespace Revolution.HUD.Entities
 
         private FrameworkElement CreateBuyBarracksButton()
         {
+            int wood, gold;
             var tooltip = GetTooltipForBuilding("Barracks", 0, 0);
 
             var button = new HudMiniActionButton(
@@ -101,6 +123,9 @@ namespace Revolution.HUD.Entities
 
         private FrameworkElement CreateBuyTownCenterButton()
         {
+            int wood = 0;
+            int gold = 0;
+            GetPriceForBuilding<TownCenter>(ref wood, ref gold);
             var tooltip = GetTooltipForBuilding("Town Center", 0, 0);
 
             var button = new HudMiniActionButton(
@@ -116,6 +141,19 @@ namespace Revolution.HUD.Entities
         private void PurchaseBuilding<T>() where T : Entity
         {
             _messenger.Send(new BuildingPurchaseEvent(typeof(T), PlayerHelper.GetGuiControlledPlayer()));
+        }
+
+        private void GetPriceForBuilding<T>(ref int wood, ref int gold) where T : Entity
+        {
+            var building = EntityManager.CreateEntity<T>();
+            var priceComp = building.GetComponent<PriceComponent>();
+            if (priceComp != null)
+            {
+                wood = priceComp.Wood;
+                gold = priceComp.Gold;
+            }
+
+            building.Destroy();
         }
     }
 }
