1f0f4e9c8bca53f9dccb6c8345d92b7a6ebfd427
Benjamin Vereczkei <EDU_HTSI_2773@sulid.hu>
2022-05-04 22:04:54 +0200

    mapData export bug fix

--- ECS/Components/GridComponent.cs
+++ ECS/Components/GridComponent.cs
@@ -9,88 +9,90 @@ using System.Threading.Tasks;
 
 namespace Revolution.ECS.Components
 {
-	public class GridComponent : Component
-	{
-		//private int width;
-		public int width;
-		//private int height;
-		public int height;
-		private float cellSize;
-		private int[,] gridArray;
+    public class GridComponent : Component
+    {
+        //private int width;
+        public int width;
+        //private int height;
+        public int height;
+        private float cellSize;
+        private int[,] gridArray;
 
-		public int[,] GridArray
-		{
-			get { return gridArray; }
-			set { gridArray = value; }
-		}
+        public int[,] GridArray
+        {
+            get { return gridArray; }
+            set { gridArray = value; }
+        }
 
-		string gridFile = "grid.txt";
+        string gridFile = "grid.txt";
 
         public GridComponent(MapData mapData)
         {
-			height = mapData.Entities.GetLength(0);
-			width = mapData.Entities.GetLength(1);
-			gridArray = new int[height, width];
+            height = mapData.Entities.GetLength(0);
+            width = mapData.Entities.GetLength(1);
+            gridArray = new int[height, width];
 
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-					gridArray[i, j] = -1;
+                    gridArray[i, j] = -1;
                 }
-			}
+            }
 
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    if (mapData.Entities[i,j] == null && mapData.Tiles[i, j].All(tile => !tile.Colliding))
+                    if (mapData.Entities[i, j] == null && mapData.Tiles[i, j].All(tile => !tile.Colliding))
                     {
-						gridArray[i,j] = 0;
+                        gridArray[i, j] = 0;
                     }
                 }
             }
+
+            ExportGrid(gridArray);
         }
 
-		//public GridComponent(int width, int height, float cellSize)
-		//{
-		//	this.width = width;
-		//	this.height = height;
-		//	this.cellSize = cellSize;
-		//	this.gridArray = new int[width, height];
+        //public GridComponent(int width, int height, float cellSize)
+        //{
+        //	this.width = width;
+        //	this.height = height;
+        //	this.cellSize = cellSize;
+        //	this.gridArray = new int[width, height];
 
-		//	string[]? lineData;
-		//	int lineElement;
+        //	string[]? lineData;
+        //	int lineElement;
 
-		//	using (StreamReader sr = new StreamReader(gridFile, Encoding.UTF8, true, width))
-		//	{
-		//		for (int i = 0; i < gridArray.GetLength(0); i++)
-		//		{
-		//			lineData = sr.ReadLine().Split(',');
-		//			for (int j = 0; j < gridArray.GetLength(1); j++)
-		//			{
-		//				gridArray[i, j] = int.Parse(lineData[j]);
-		//			}
-		//		}
-		//	}
-		//}
+        //	using (StreamReader sr = new StreamReader(gridFile, Encoding.UTF8, true, width))
+        //	{
+        //		for (int i = 0; i < gridArray.GetLength(0); i++)
+        //		{
+        //			lineData = sr.ReadLine().Split(',');
+        //			for (int j = 0; j < gridArray.GetLength(1); j++)
+        //			{
+        //				gridArray[i, j] = int.Parse(lineData[j]);
+        //			}
+        //		}
+        //	}
+        //}
 
-		public void ExportGrid(int[,] gridArray)
-		{
-			return;
+        public void ExportGrid(int[,] gridArray)
+        {
+            //return;
 
-			using (StreamWriter sw = new StreamWriter("grid_output.txt"))
-			{
-				for (int i = 0; i < gridArray.GetLength(0); i++)
-				{
-					for (int j = 0; j < gridArray.GetLength(1); j++)
-					{
-						sw.Write(gridArray[i, j]);
-						sw.Write(',');
-					}
-					sw.WriteLine();
-				}
-			}
-		}
-	}
+            using (StreamWriter sw = new StreamWriter("grid_output.txt"))
+            {
+                for (int i = 0; i < gridArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < gridArray.GetLength(1); j++)
+                    {
+                        sw.Write(gridArray[j, i]);
+                        sw.Write(',');
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+    }
 }
